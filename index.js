a730f09654d941805eb4a7fc668c2ff772184a79
Vaibhav Patel <vp932712@gmail.com>
2017-11-14 21:11:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array){
-array.foEach(changeCompletely(element){
-return element;
-})
+
 }
 
 // Add your changeCompletely() function here:
 function changeCompletely(array){
-array.forEach(function(element){
-  return element+element;
-})
+
 }
