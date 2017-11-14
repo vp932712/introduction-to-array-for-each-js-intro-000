30970c1148c2c949955d0259242bacda2830d4cc
Vaibhav Patel <vp932712@gmail.com>
2017-11-14 21:14:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array){
-
+arra.forEach(function (element){
+  return "yumm"+element;
+})
 }
 
 // Add your changeCompletely() function here:
