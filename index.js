cb7956f6534448fcba5eeddd34b77b07685e68a5
Vaibhav Patel <vp932712@gmail.com>
2017-11-14 21:44:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Add your doToElementsInArray() function here:
+callback = function(fruit) {
+  return `Mmmm, ${fruit}!!!`;
+};
 
+
+function doToElementsInArray(array, callback){
+array.forEach(callback)
+
+
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
